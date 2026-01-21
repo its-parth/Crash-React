157d90a405f77c8025e795aa820f71f51a8b1214
Parth Magar <parthmagar789@gmail.com>
2026-01-21 18:01:05 +0530

    intermediate1 setup done for form handling

---
+++ intermediate1-form-handling/src/App.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(0);
+  useEffect(() => {
+
+  }, []);
+  return (
+    <div>Parth Magar Is Awesome Developer</div>
+  )
+}
+
+export default App
