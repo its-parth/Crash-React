50f467779be31710cd8550a795aa61ef6e33ef36
Parth Magar <parthmagar789@gmail.com>
2026-02-13 01:39:23 +0530

    advance5 fetching data directly done

--- advance5-ecommerce-project/src/Pages/Home.jsx
+++ advance5-ecommerce-project/src/Pages/Home.jsx
@@ -1,18 +1,46 @@
-import { products } from '../data'
+import { useEffect, useState } from 'react'
 import Product from '../components/Product'
+import Loader from '../components/Loader';
 
 const Home = () => {
-  
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch("https://fakestoreapi.com/products");
+        const data = await response.json();
+        setProducts(data);
+        setLoading(false);
+      } catch {
+        setProducts([]);
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, [])
   return (
-    <div className='w-screen flex justify-center'>
-      <div className="products-container w-full max-w-11/12 flex flex-wrap gap-6 sm:gap-6 py-8 justify-center px-1">
-          {
-            products.map(product => {
-              return <Product key={product.id} product={product} />
-            })
-          }
-      </div>
+    <div className='flex-1 flex'>
+      {
+        loading ? 
+        <div className='flex items-center min-h-full'>
+          <Loader />
+        </div>
+        :
+        <div className='w-screen flex justify-center'>
+          <div className="products-container w-full max-w-11/12 flex flex-wrap gap-6 sm:gap-6 py-8 justify-center px-1">
+            {
+              products.map(product => {
+                return <Product key={product.id} product={product} />
+              })
+            }
+          </div>
+        </div>
+      }
+
     </div>
+
   )
 }
 
